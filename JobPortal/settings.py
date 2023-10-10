3441b8a1256f46c6fb8df6c347cb829dd6f5fdfb
Gulrez Alam <egulrezalam@gmail.com>
2023-10-11 05:21:54 +0530

    Add .env file to .gitignore

--- JobPortal/settings.py
+++ JobPortal/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,10 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'aralxt83cxseu%+7%(-wx3qrtf+xjrq64zg9lxw&88coqr3ha*'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG" , default=False, cast=bool)
 
 ALLOWED_HOSTS = ['.vercel.app', '.netlify.app', '127.0.0.1']
 
@@ -65,8 +66,12 @@ WSGI_APPLICATION = 'JobPortal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config("NAME"),
+        'USER': config("USER"),
+        'HOST': config("HOST"),
+        'PASSWORD': config("PASSWORD"),
+        'PORT': '',
     }
 }
 
