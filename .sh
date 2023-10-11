a6f1bd7f0a816bc4cea01971e732f3d5e65f5ec3
Gulrez Alam <egulrezalam@gmail.com>
2023-10-11 05:50:44 +0530

    added .sh file

---
+++ .sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Build the project.
+echo "Building..."
+python -m pip install -r requirements.txt
+
+echo "Making migrations..."
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+# Collect static files
+python manage.py collectstatic --noinput --clear
